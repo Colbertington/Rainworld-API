20e5c92db410025f0b096a0af4c0fdbc4c6bde8a
Colby <colbymcfaul27@gmail.com>
2025-05-14 10:09:43 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors'); // <-- Add this line
+const app = express();
+const PORT = 3000;
+
+app.use(cors()); // <-- And this line
+
+app.get('/api/rainworld', (req, res) => {
+  fs.readFile('rainworld-api.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Could not read data file' });
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Rain World API running at http://localhost:${PORT}/api/rainworld`);
+});
